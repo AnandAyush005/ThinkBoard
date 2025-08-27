2b903857295d6bf61a5fabe606fa4d651f597d13
AnandAyush005 <anandayush233@gmail.com>
2025-08-27 18:58:18 +0530

    check

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,11 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
-
-// app.use((req,res,next)=>{
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// })
+// checking the server
 
 app.use("/api/notes", router);
 
